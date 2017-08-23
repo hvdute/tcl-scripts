e6b375dd3f54f218e8b7f33b26f3f87c5280b952
Dung Hoang Van <hvd.ute@gmail.com>
2017-08-23 17:56:09 +0700

    Add hello world example

---
+++ helloworld.tcl
@@ -0,0 +1,10 @@
+#!/usr/bin/tclsh
+puts "Hello world!"
+#puts $env(PATH)
+#puts $tcl_pkgPath
+set a "Hoang Van Dung"
+if true {
+    puts [expr 3 + 2 + 10000 ]
+    puts "My name is: $a"
+}
+# this is a comment 
